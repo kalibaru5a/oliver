3dcbd51993d8349cd23ef7a00ed402a7a7fdceb4
kalibaru5a <96695972+kalibaru5a@users.noreply.github.com>
2023-01-24 00:31:10 +0700

    Add files via upload

---
+++ kalibaru5a200x300.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'b05ef0f3cfdf86f368e370e816b97c96',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/b05ef0f3cfdf86f368e370e816b97c96/invoke.js"></scr' + 'ipt>');
---
+++ kalibaru5a90x728.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'd07de303de40e2946e5b9f3b6b7321c2',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/d07de303de40e2946e5b9f3b6b7321c2/invoke.js"></scr' + 'ipt>');
